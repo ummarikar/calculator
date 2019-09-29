1dc98cd90bf48624036bd71adc4dbcfec82a1265
K1630549 <umar.marikar@kcl.ac.uk>
2019-09-29 02:39:35 +0100

    initial commit

---
+++ src/calc.y
@@ -0,0 +1,34 @@
+%start Expr
+%%
+Expr -> Result<u64, ()>:
+      Term 'PLUS' Expr { Ok($1? + $3?) }
+    | Term 'SUB' Expr { Ok($1? - $3?) }
+    | Term { $1 }
+    ;
+
+Term -> Result<u64, ()>:
+      Factor 'MUL' Term { Ok($1? * $3?) }
+    | Factor 'DIV' Term { Ok($1? / $3?) }
+    | Factor { $1 }
+    ;
+
+Factor -> Result<u64, ()>:
+      'LBRACK' Expr 'RBRACK' { $2 }
+    | 'INT'
+      {
+          let v = $1.map_err(|_| ())?;
+          parse_int($lexer.lexeme_str(&v))
+      }
+    ;
+%%
+// Any functions here are in scope for all the grammar actions above.
+
+fn parse_int(s: &str) -> Result<u64, ()> {
+    match s.parse::<u64>() {
+        Ok(val) => Ok(val),
+        Err(_) => {
+            eprintln!("{} cannot be represented as a u64", s);
+            Err(())
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::io::{self, BufRead, Write};
+
+use lrlex::lrlex_mod;
+use lrpar::lrpar_mod;
+
+// Using `lrlex_mod!` brings the lexer for `calc.l` into scope.
+lrlex_mod!(calc_l);
+// Using `lrpar_mod!` brings the lexer for `calc.l` into scope.
+lrpar_mod!(calc_y);
+
+fn main() {
+    // We need to get a `LexerDef` for the `calc` language in order that we can lex input.
+    let lexerdef = calc_l::lexerdef();
+    let stdin = io::stdin();
+    loop {
+        print!(">>> ");
+        io::stdout().flush().ok();
+        match stdin.lock().lines().next() {
+            Some(Ok(ref l)) => {
+                if l.trim().is_empty() {
+                    continue;
+                } else if l == "exit" {
+                    break;
+                }
+                // Now we create a lexer with the `lexer` method with which we can lex an input.
+                let mut lexer = lexerdef.lexer(l);
+                // Pass the lexer to the parser and lex and parse the input.
+                let (res, errs) = calc_y::parse(&mut lexer);
+                for e in errs {
+                    println!("{}", e.pp(&lexer, &calc_y::token_epp));
+                }
+                match res {
+                    Some(Ok(r)) => println!("{}", r),
+                    _ => eprintln!("Unable to evaluate expression.")
+                }
+            }
+            _ => break
+        }
+    }
+}
